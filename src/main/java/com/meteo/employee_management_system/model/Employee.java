ad70ab470d10cf1e18fc83273bd98743cf16c3e9
ijakhongir13 <jakhongir.islamov.13@gmail.com>
2021-08-18 17:29:44 +0500

    updated update_employee.html, employee_by_id.html and new_em  pages

--- src/main/java/com/meteo/employee_management_system/model/Employee.java
+++ src/main/java/com/meteo/employee_management_system/model/Employee.java
@@ -36,4 +36,24 @@ public class Employee {
     private String nationality;
     @Column(name = "phone_no")
     private String phoneNo;
+    @Column(name = "work_department")
+    private String workDepartment;
+    @Column(name = "work_position")
+    private String workPosition;
+    @Column(name = "edu_institution")
+    private String eduInstitution;
+    @Column(name = "edu_country")
+    private String eduCountry;
+    @Column(name = "edu_speciality")
+    private String eduSpeciality;
+    @Column(name = "edu_type")
+    private String eduType;
+    @Column(name = "edu_addmission_date")
+    private String eduAdmissionDate;
+    @Column(name = "edu_graduation_date")
+    private String eduGraduationDate;
+    @Column(name = "edu_diploma_specs")
+    private String eduDiplomaSpecs;
+    @Column(name = "marital_status")
+    private String maritalStatus;
 }
